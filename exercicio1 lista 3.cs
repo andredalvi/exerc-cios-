b28246186bc6e1bddafef4813665bed4cf75e9a7
andredalvi <andrelopesdalvi@gmail.com>
2026-02-07 18:43:53 -0300

    Update exercicio1 lista 3.cs

--- exercicio1 lista 3.cs
+++ exercicio1 lista 3.cs
@@ -7,18 +7,18 @@ namespace exercicio1
         class Retangulo
         {
             private float base_ret;
-            private float altura_ret;
+            private float altura_ret;                      //declarando as variaveis do objeto
 
             public Retangulo()
             {
-                base_ret = 0;
+                base_ret = 0;                  // construtor default
                 altura_ret = 0;
             }
 
             public Retangulo(float b, float h)
             {
                 base_ret = b;
-                altura_ret = h;
+                altura_ret = h;            //construtor completo
             }
 
             public void definir_base(float comando)
@@ -33,7 +33,7 @@ namespace exercicio1
 
             public float pegar_base()
             {
-                return base_ret;
+                return base_ret;                                    //metodos get e set para cada variavel
             }
 
             public float pegar_altura()
@@ -49,7 +49,7 @@ namespace exercicio1
             public void Mostrar_Dados()
             {
                 Console.WriteLine("Base: " + base_ret);
-                Console.WriteLine("Altura: " + altura_ret);
+                Console.WriteLine("Altura: " + altura_ret);                //metodo para imprimir cada medida do retangulo
                 Console.WriteLine("Area: " + pegar_area());
             }
         }
