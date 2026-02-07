df613a438e1dcdf4e0b6dc28c0d2df33966ee068
andredalvi <andrelopesdalvi@gmail.com>
2026-02-07 17:57:40 -0300

    Add files via upload

---
+++ exercicio2 lista 3.cs
@@ -0,0 +1,12 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        for (int numero = 2; numero <= 100; numero += 2)
+        {
+            Console.WriteLine(numero);
+        }
+    }
+}
