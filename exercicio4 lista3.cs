d94e3a92ae7cb3c3c253480fee23227227b41f95
andredalvi <andrelopesdalvi@gmail.com>
2026-02-07 18:52:10 -0300

    Update exercicio4 lista3.cs

--- exercicio4 lista3.cs
+++ exercicio4 lista3.cs
@@ -1,19 +1,19 @@
         class Animal
         {
             private string nome;
-            private string tipo;
+            private string tipo;                        // criando objeto e as variaveis
 
             public Animal()
             {
                 nome = "";
-                tipo = "peixe";
+                tipo = "peixe";                //construtor default
             }
 
             public Animal(string nome_, string tipo_)
             {
                 nome = nome_;
 
-                if (tipo_ == "cachorro" || tipo_ == "gato" || tipo_ == "peixe")
+                if (tipo_ == "cachorro" || tipo_ == "gato" || tipo_ == "peixe")                // construtor padrão com o filtro de tipo, limitando a somente 3 tipos, ou convertendo qualquer outra entrada para peixe
                 {
                     tipo = tipo_;
                 }
@@ -30,7 +30,7 @@
 
             public void definir_tipo(string comando)
             {
-                if (comando == "cachorro" || comando == "gato" || comando == "peixe")
+                if (comando == "cachorro" || comando == "gato" || comando == "peixe")                        //metodo definir com if para limitar os tipos
                 {
                     tipo = comando;
                 }
@@ -42,7 +42,7 @@
 
             public string pegar_nome()
             {
-                return nome;
+                return nome;                        //metodos gets
             }
 
             public string pegar_tipo()
